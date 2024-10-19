d498d7b75c21e82f1e9d251f23d0d39287997ae8
BuildTools <unconfigured@null.spigotmc.org>
2024-10-19 13:20:56 +0200

    commited

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = in.nextLine();
+        System.out.print("Enter your massage: ");
         String input = in.nextLine();
-        System.out.println("User: " + input);
+        System.out.println(name + ": " + input);
     }
 }
